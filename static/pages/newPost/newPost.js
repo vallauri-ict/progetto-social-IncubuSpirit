42f698dfad6e622b0f97f1c3003f75f7506d22ca
rjxky <r.marcarino.0814@vallauri.edu>
2021-01-20 21:43:01 +0100

    commit

--- static/pages/newPost/newPost.js
+++ static/pages/newPost/newPost.js
@@ -2,12 +2,20 @@
 
 $(document).ready(function(){
     let _btnPublish=$("#btnPublish");
+    $("#video-preview").hover( hoverVideo, hideVideo );
 
     _btnPublish.on("click",function(){
         alert("hello boya");
     })
 })
 
+function hoverVideo(e) {  
+  $(this).get(0).play(); 
+}
+
+function hideVideo(e) {
+  $(this).get(0).pause();
+}
 
 function showPreview(event){
     if(event.target.files.length > 0){
@@ -17,3 +25,26 @@ function showPreview(event){
       preview.style.display = "block";
     }
   }
+
+  var mimeType=this.files[0]['type'];//mimeType=image/jpeg or application/pdf etc...
+
+
+//ie image/jpeg will be ['image','jpeg'] and we keep the first value
+    if(mimeType.split('/')[0] === 'image'){
+       imageIsLoaded;
+    }
+
+    if(mimeType.split('/')[0] === 'video'){
+      videoIsLoaded;
+   }
+
+    function imageIsLoaded(e) {
+        result = e.target.result;
+        $('#video-preview').attr('src', '');
+        $('#image-preview').attr('src', result);
+    };
+    function videoIsLoaded(e) {
+      result = e.target.result;
+      $('#video-preview').attr('src', '');
+      $('#image-preview').attr('src', result);
+  };
