c20541d34bd2830c6b08a68ab37a310985eeee41
rjxky <r.marcarino.0814@vallauri.edu>
2021-01-25 01:28:37 +0100

    Commit

--- static/pages/newPost/newPost.js
+++ static/pages/newPost/newPost.js
@@ -1,88 +1,63 @@
 "use strict";
 
-let _posttype;
-
-    _btnPublish.on("click", controllaLogin);
-
-    function controllaLogin() {
-      let tokenRequest=inviaRichiesta("POST","/api/controllaToken");
-      tokenRequest.fail(function() {
-          console.log("Not logged in.");
-      });
-      tokenRequest.done(function(data){
-          if(data["ris"]!="missingToken")
-          {
-              console.log("POST PUBBLICATO");
-          }
-      });
-    }
-
-
-  function errore(jqXHR, testStatus, strError) {
-      if (jqXHR.status == 0)
-      {
-          openSnackbar("Connection refused or Server timeout");
-      }
-      else if(jqXHR.status == 403)
-      {
-          openSnackbar("Errore durante il login!");
-      }
-      else if (jqXHR.status == 200)
-      {
-          openSnackbar("Data format uncorrect: " + jqXHR.responseText);
-      }
-      else
-      {
-          openSnackbar("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
-      }
-  }
 $(document).ready(function () {
   let _btnPublish = $("#btnPublish");
   let _descrizione=$("#description");
+  let _posttype;
+  let currentUser;
   $("#video-preview").hover(hoverVideo, hideVideo);
 
-  _btnPublish.on("click", function () {
-    if(_descrizione.attr("value")!="")
-    let request = inviaRichiesta("POST", "/api/newpost",{
-        
+  _btnPublish.on("click", cercaUsername);
+
+  function hoverVideo(e) {
+    $(this).get(0).play();
+  };
+
+  function hideVideo(e) {
+    $(this).get(0).pause();
+  };
+
+  function cercaUsername() {
+    let tokenRequest=inviaRichiesta("POST","/api/controllaToken");
+    tokenRequest.fail(function() {
+        console.log("Not logged in.");
     });
-    request.fail(function (jqXHR, test_status, str_error) {
-      if (jqXHR.status == 401) { // unauthorized
-        _lblErrore.show();
-      } else
-        errore(jqXHR, test_status, str_error);
+    tokenRequest.done(function(data){
+        if(data["ris"]!="missingToken")
+        {
+            currentUser=data["username"];
+            console.log("ciao");
+        }
     });
-    request.done(function (data) {
-      center.attr("src", "/pages/feed/feed.html");
-    });
-  });
-});
+    /*let request = inviaRichiesta("POST", "/api/getUsername");
+    request.fail(errore);
 
-function hoverVideo(e) {
-  $(this).get(0).play();
-};
+    request.done(function(data)
+    {
+        console.log(data["username"]);
+        openSnackbar(data["username"]);
+    });*/
+  }
 
-function hideVideo(e) {
-  $(this).get(0).pause();
-};
+  function showPreview(event) {
+    _posttype="";
+    var src = URL.createObjectURL(event.target.files[0]);
+    if (event.target.files.length > 0) {
+      var mimeType = event.target.files[0]['type'];
+      if (mimeType.split('/')[0] === 'image') {
+        _posttype="image";
+        document.getElementById("image-preview").style.display = "block";
+        document.getElementById("image-preview").src = src;
+        document.getElementById("video-preview").style.display = "none";
+      }
 
-function showPreview(event) {
-  _posttype="";
-  var src = URL.createObjectURL(event.target.files[0]);
-  if (event.target.files.length > 0) {
-    var mimeType = event.target.files[0]['type'];
-    if (mimeType.split('/')[0] === 'image') {
-      _posttype="image";
-      document.getElementById("image-preview").style.display = "block";
-      document.getElementById("image-preview").src = src;
-      document.getElementById("video-preview").style.display = "none";
-    }
-
-    if (mimeType.split('/')[0] === 'video') {
-      _posttype="video";
-      document.getElementById("image-preview").style.display = "none";
-      document.getElementById("video-preview").style.display = "block";
-      document.getElementById("video-preview").src = src;
+      if (mimeType.split('/')[0] === 'video') {
+        _posttype="video";
+        document.getElementById("image-preview").style.display = "none";
+        document.getElementById("video-preview").style.display = "block";
+        document.getElementById("video-preview").src = src;
+      }
     }
   }
-};
+});
+
